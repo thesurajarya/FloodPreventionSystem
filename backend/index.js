02eae6fe0a0371ea7e965a8230528cae6b78984d
Suraj Arya <s.arya844811@gmail.com>
2026-03-09 15:38:52 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
